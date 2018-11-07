fc0ea35f648a25a8cf6617a5159403a90807b024
Joe Stokes <joestokes1994@gmail.com>
2018-11-07 12:09:41 +0000

    added the add recipe form

--- app.py
+++ app.py
@@ -16,10 +16,27 @@ def get_recipes():
     return render_template("recipes.html", recipes=all_recipes)
 #Use the find() function to return all of the recipes from the Mongo Collection
 
-@app.route('/show_recipe/<recipe_id>')
+
+@app.route('/show_recipe/<recipe_id>/')
 def show_recipe(recipe_id):
     recipe_id = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template("show_recipe.html", recipe=recipe_id)
+
+
+@app.route('/add_recipe/', methods=['GET', 'POST'])
+def add_recipe():
+    _cuisines = mongo.db.cuisines.find()
+    cuisine_list = [cuisine for cuisine in _cuisines]
+    return render_template("addrecipe.html", cuisines=cuisine_list)
+
+
+@app.route('/insert_recipe/', methods=['GET','POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    dictionary = recipes.insert_one(request.form.to_dict())
+    print(dictionary)
+    return redirect(url_for("get_recipes"))
+    
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
